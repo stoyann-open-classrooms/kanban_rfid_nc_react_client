acdfe8cc0b20ec13e97381420fcd4b491ae22ea4
Stoyann Velten <stoyann.openclassrooms@gmail.com>
2022-11-16 11:30:01 +1100

    add env file and set base URL

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log(process.env.REACT_APP_APP_BASE_URL);
   return (
   
 
     <div className='bg-purple-500'>
     <h1 className="text-6xl font-bold ">
-    Hello kanban!
+  {process.env.REACT_APP_APP_BASE_URL}
   </h1>
 
   </div>
